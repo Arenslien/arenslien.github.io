9c06db2083bafcb6044d2326b294ee0b57c56f4c
Arenslien <arenslien@gmail.com>
2023-03-15 14:36:32 +0900

    router 준비

--- github_page/src/App.js
+++ github_page/src/App.js
@@ -4,23 +4,31 @@ import './App.css';
 function App() {
   // Variable
   let title = "SBTI(Siloam Bible Type Indicator)";
-  let current_answer_num = 1;
+  let [currentAnswerNum, setAnswerNum] = useState(1);
 
   // State
-  let [questions, answers] = useState([
+  let [questions] = useState([
     "Question 1", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6", "Question 7", "Question 8", "Question 9", "Question 10", "Question 11", "Question 12", "Question 13", "Question 14", "Question 15", "Question 16", "Question 17", "Question 18", "Question 19", "Question 20",
   ]);
+
+  // Score
+  let e_score = useState(0);
+  let s_score = useState(0);
+  let t_score = useState(0);
+  let j_score = useState(0);
 
   // useRef
   const nextRef = useRef([]);
 
   function nextQuestion(idx) {
-    nextRef.current[idx+1].scrollIntoView({ behavior: "smooth" });
-    current_answer_num++;
+    if (idx !== 20) {
+      nextRef.current[idx+1].scrollIntoView({ behavior: "smooth" });
+      setAnswerNum(currentAnswerNum + 1);
+    }
   }
 
   return (
-    <div className="App"> // start Tag
+    <div className="App">
       <div class="black-nav">
         Arenslien's page
       </div>
@@ -34,7 +42,7 @@ function App() {
           {questions.map(function (question, idx) {
             return (
               <div class="question-box" ref={element => nextRef.current[idx] = element}>
-                <h2>{current_answer_num}/20</h2>
+                <h2 class="">{currentAnswerNum}/20</h2>
                 <h2> {question} </h2>
                 <button class="answer-button" onClick={() => nextQuestion(idx)}> content 1 </button>
                 <br></br>
